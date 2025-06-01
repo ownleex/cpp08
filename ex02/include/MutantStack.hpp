04f308dd303a772c4386836edd2422747dc57723
ownleex <145456737+ownleex@users.noreply.github.com>
2025-06-01 23:33:48 +0200

    Ajout de l'implémentation du template MutantStack dans MutantStack.tpp et mise à jour de MutantStack.hpp pour inclure ce fichier.

--- ex02/include/MutantStack.hpp
+++ ex02/include/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 21:56:08 by ayarmaya          #+#    #+#             */
-/*   Updated: 2025/05/30 21:56:58 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2025/06/01 23:12:22 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,80 +45,6 @@ public:
 };
 
 // Implémentation du template dans le header
-
-template<typename T, typename Container>
-MutantStack<T, Container>::MutantStack() : std::stack<T, Container>()
-{
-}
-
-template<typename T, typename Container>
-MutantStack<T, Container>::MutantStack(const MutantStack& other) : std::stack<T, Container>(other)
-{
-}
-
-template<typename T, typename Container>
-MutantStack<T, Container>& MutantStack<T, Container>::operator=(const MutantStack& other)
-{
-    if (this != &other)
-    {
-        std::stack<T, Container>::operator=(other);
-    }
-    return *this;
-}
-
-template<typename T, typename Container>
-MutantStack<T, Container>::~MutantStack()
-{
-}
-
-// Itérateurs normaux
-template<typename T, typename Container>
-typename MutantStack<T, Container>::iterator MutantStack<T, Container>::begin()
-{
-    return this->c.begin();
-}
-
-template<typename T, typename Container>
-typename MutantStack<T, Container>::iterator MutantStack<T, Container>::end()
-{
-    return this->c.end();
-}
-
-template<typename T, typename Container>
-typename MutantStack<T, Container>::const_iterator MutantStack<T, Container>::begin() const
-{
-    return this->c.begin();
-}
-
-template<typename T, typename Container>
-typename MutantStack<T, Container>::const_iterator MutantStack<T, Container>::end() const
-{
-    return this->c.end();
-}
-
-// Itérateurs inverses
-template<typename T, typename Container>
-typename MutantStack<T, Container>::reverse_iterator MutantStack<T, Container>::rbegin()
-{
-    return this->c.rbegin();
-}
-
-template<typename T, typename Container>
-typename MutantStack<T, Container>::reverse_iterator MutantStack<T, Container>::rend()
-{
-    return this->c.rend();
-}
-
-template<typename T, typename Container>
-typename MutantStack<T, Container>::const_reverse_iterator MutantStack<T, Container>::rbegin() const
-{
-    return this->c.rbegin();
-}
-
-template<typename T, typename Container>
-typename MutantStack<T, Container>::const_reverse_iterator MutantStack<T, Container>::rend() const
-{
-    return this->c.rend();
-}
+#include "MutantStack.tpp"
 
 #endif
