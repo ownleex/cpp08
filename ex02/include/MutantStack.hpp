953e3a8404f4edf917cb2806d34a23eb538e4f61
ownleex <145456737+ownleex@users.noreply.github.com>
2025-06-02 15:54:20 +0200

    Mise à jour de MutantStack.hpp pour simplifier l'implémentation des constructeurs et des méthodes d'itération, tout en ajoutant des commentaires explicatifs.

--- ex02/include/MutantStack.hpp
+++ ex02/include/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 21:56:08 by ayarmaya          #+#    #+#             */
-/*   Updated: 2025/06/01 23:12:22 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2025/06/02 15:53:41 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,31 +20,32 @@ template<typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container>
 {
 public:
-    // Types d'itérateurs
+    // Types d'itérateurs - on utilise ceux du conteneur sous-jacent
     typedef typename Container::iterator iterator;
     typedef typename Container::const_iterator const_iterator;
     typedef typename Container::reverse_iterator reverse_iterator;
     typedef typename Container::const_reverse_iterator const_reverse_iterator;
 
     // Constructeurs et destructeur (forme canonique)
-    MutantStack();
-    MutantStack(const MutantStack& other);
-    MutantStack& operator=(const MutantStack& other);
-    ~MutantStack();
+    MutantStack() {}
+    MutantStack(const MutantStack& other) : std::stack<T, Container>(other) {}
+    MutantStack& operator=(const MutantStack& other) {
+        if (this != &other)
+            std::stack<T, Container>::operator=(other);
+        return *this;
+    }
+    ~MutantStack() {}
 
-    // Fonctions d'itération
-    iterator begin();
-    iterator end();
-    const_iterator begin() const;
-    const_iterator end() const;
+    // Fonctions d'itération - on expose simplement les itérateurs du conteneur sous-jacent
+    iterator begin() { return this->c.begin(); }
+    iterator end() { return this->c.end(); }
+    const_iterator begin() const { return this->c.begin(); }
+    const_iterator end() const { return this->c.end(); }
     
-    reverse_iterator rbegin();
-    reverse_iterator rend();
-    const_reverse_iterator rbegin() const;
-    const_reverse_iterator rend() const;
+    reverse_iterator rbegin() { return this->c.rbegin(); }
+    reverse_iterator rend() { return this->c.rend(); }
+    const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+    const_reverse_iterator rend() const { return this->c.rend(); }
 };
-
-// Implémentation du template dans le header
-#include "MutantStack.tpp"
 
 #endif
